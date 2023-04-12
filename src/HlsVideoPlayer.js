a016de0c1def91698084144fad21450c6962b419
ip-001 <ip-001@thinkpad.local>
2023-04-12 10:45:36 +0800

    Setup hls-video-player web component

--- src/HlsVideoPlayer.js
+++ src/HlsVideoPlayer.js
@@ -1,33 +1,81 @@
 import { html, css, LitElement } from 'lit';
+import Hls from 'hls.js';
 
 export class HlsVideoPlayer extends LitElement {
   static styles = css`
     :host {
+      width: 100%;
+      display: grid;
+      grid-template-columns: 1fr;
+      grid-template-rows: 1fr auto;
+      grid-template-areas:
+        'video'
+        'info ';
+      box-sizing: border-box;
+    }
+
+    video {
+      grid-area: video;
+      width: 100%;
+      height: auto;
+      box-sizing: border-box;
+    }
+
+    main {
+      grid-area: info;
+      width: 100%;
+      height: auto;
+      padding: 1.25rem 0;
+      box-sizing: border-box;
+    }
+
+    h6 {
+      font-family: 'Roboto', sans-serif;
+      font-size: 16px;
+      font-weight: 1.25rem;
+      letter-spacing: 0.15px;
+      margin: 0;
+      color: black;
+    }
+
+    span {
+      font-family: 'Roboto', sans-serif;
+      font-size: 0.75rem;
+      font-weight: 400;
       display: block;
-      padding: 25px;
-      color: var(--hls-video-player-text-color, #000);
+      padding: 0.5rem 0.125rem;
+      padding-bottom: 0.125rem;
+      color: var(--secondary-color);
     }
   `;
 
   static properties = {
-    header: { type: String },
-    counter: { type: Number },
+    name: { type: String },
+    src: { type: String },
   };
 
-  constructor() {
-    super();
-    this.header = 'Hey there';
-    this.counter = 5;
-  }
+  firstUpdated() {
+    const player = this.shadowRoot.querySelector('video');
 
-  __increment() {
-    this.counter += 1;
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(this.src);
+      hls.attachMedia(player);
+    } else if (player.canPlayType('application/vnd.apple.mpegurl')) {
+      player.src = this.src;
+    }
   }
 
   render() {
     return html`
-      <h2>${this.header} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <video controls controlsList="nodownload">
+        Your device does not support playing HTTP Live Streaming (HLS) videos.
+      </video>
+
+      <main>
+        <h6>${this.name}</h6>
+        <span>video</span>
+      </main>
     `;
   }
 }
